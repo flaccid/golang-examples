89d6522bdf73e90e16491dab30238987a98f4c5c
Chris Fordham <chris@fordham-nagy.id.au>
2019-01-21 11:37:31 +1100

    Working azure ad httpd with file sessions.

--- httpoauthazuread.go
+++ httpoauthazuread.go
@@ -5,6 +5,7 @@ import (
 	"encoding/gob"
 	"errors"
 	"fmt"
+	"math"
 	"math/rand"
 	"net/http"
 	"net/url"
@@ -22,19 +23,15 @@ import (
 )
 
 type OAuthApp struct {
-	Config oauth2.Config
-	State  string
-  TenantId string
-}
-
-type Profile struct {
-	ID, DisplayName, ImageURL string
+	Config   oauth2.Config
+	State    string
+	TenantId string
 }
 
 const (
 	// set the default sessions keys
-	defaultSessionID        = "default"
-	oauthTokenSessionKey    = "oauth_token"
+	defaultSessionID     = "default"
+	oauthTokenSessionKey = "oauth_token"
 
 	// This key is used in the OAuth flow session to store the URL to redirect the
 	// user to after the OAuth flow is complete.
@@ -44,16 +41,18 @@ const (
 var (
 	letterRunes   = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	encryptionKey = []byte(securecookie.GenerateRandomKey(64))
-	SessionStore  = sessions.NewCookieStore(encryptionKey)
-	app           = OAuthApp{
-				Config: oauth2.Config{
-					Scopes:   []string{"User.Read"},
-      					Endpoint: oauth2.Endpoint{
-      						AuthURL:  "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
-  						TokenURL: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
-      					},
-				},
-			}
+
+	Store = sessions.NewFilesystemStore(os.TempDir(), encryptionKey)
+
+	App = OAuthApp{
+		Config: oauth2.Config{
+			Scopes: []string{"User.Read"},
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
+				TokenURL: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+			},
+		},
+	}
 )
 
 func RandStringRunes(n int) string {
@@ -90,13 +89,13 @@ func publicHandler(w http.ResponseWriter, r *http.Request) {
 
 // logoutHandler clears the default session.
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := SessionStore.New(r, defaultSessionID)
+	session, err := Store.New(r, defaultSessionID)
 	if err != nil {
-		log.Error(err, "could not get default session: %v", err)
+		log.Errorf("could not get default session: ", err)
 	}
 	session.Options.MaxAge = -1 // Clear session.
 	if err := session.Save(r, w); err != nil {
-		log.Error(err, "could not save session: %v", err)
+		log.Errorf("could not save session: ", err)
 	}
 	redirectURL := r.FormValue("redirect")
 	if redirectURL == "" {
@@ -106,7 +105,7 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func secretHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := SessionStore.Get(r, defaultSessionID)
+	session, err := Store.Get(r, defaultSessionID)
 	if err != nil {
 		log.Error(err, "could not get default session: %v", err)
 	}
@@ -126,7 +125,7 @@ func secretHandler(w http.ResponseWriter, r *http.Request) {
 func azureADLoginHandler(w http.ResponseWriter, r *http.Request) {
 	sessionID := uuid.Must(uuid.NewV4()).String()
 
-	oauthFlowSession, err := SessionStore.New(r, sessionID)
+	oauthFlowSession, err := Store.New(r, sessionID)
 	if err != nil {
 		log.Error(err, "could not create oauth session: %v", err)
 	}
@@ -140,20 +139,20 @@ func azureADLoginHandler(w http.ResponseWriter, r *http.Request) {
 	oauthFlowSession.Values[oauthFlowRedirectKey] = redirectURL
 
 	if err := oauthFlowSession.Save(r, w); err != nil {
-		log.Error(err, "could not save session: %v", err)
+		log.Error(err, "could not save oauth flow session: %v", err)
 	}
 
 	// Use the session ID for the "state" parameter.
 	// This protects against CSRF (cross-site request forgery).
 	// See https://godoc.org/golang.org/x/oauth2#Config.AuthCodeURL for more detail.
-	url := app.Config.AuthCodeURL(sessionID, oauth2.ApprovalForce,
+	url := App.Config.AuthCodeURL(sessionID, oauth2.ApprovalForce,
 		oauth2.AccessTypeOnline)
 	log.Debug("url", url)
 	http.Redirect(w, r, url, http.StatusFound)
 }
 
 func azureADCallbackHandler(w http.ResponseWriter, r *http.Request) {
-	oauthFlowSession, err := SessionStore.Get(r, r.FormValue("state"))
+	oauthFlowSession, err := Store.Get(r, r.FormValue("state"))
 	if err != nil {
 		log.Error(err, "invalid state parameter. try logging in again.")
 		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
@@ -162,19 +161,19 @@ func azureADCallbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	redirectURL, ok := oauthFlowSession.Values[oauthFlowRedirectKey].(string)
 	log.Debug(redirectURL)
-	// Validate this callback request came from the app.
+	// Validate this callback request came from the App.
 	if !ok {
 		log.Error("invalid state parameter. try logging in again.")
 		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
 	}
 
 	code := r.FormValue("code")
-	tok, err := app.Config.Exchange(context.Background(), code)
+	tok, err := App.Config.Exchange(context.Background(), code)
 	if err != nil {
 		log.Error(err, "could not get auth token: %v", err)
 	}
 
-	session, err := SessionStore.New(r, defaultSessionID)
+	session, err := Store.New(r, defaultSessionID)
 	if err != nil {
 		log.Error(err, "could not get default session: %v", err)
 	}
@@ -184,49 +183,54 @@ func azureADCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	// set user as authenticated
 	session.Values["authenticated"] = true
 
-
 	if err := session.Save(r, w); err != nil {
 		log.Error(err, "could not save session: %v", err)
 	}
 	log.Debug("session values", session.Values)
-  log.Debug(session.Values["oauth_token"])
+	log.Debug(session.Values["oauth_token"])
 
 	http.Redirect(w, r, redirectURL, http.StatusFound)
 }
 
-func Serve(clientId string, clientSecret string, redirectUrl string, tenantId string) {
-	app.Config.ClientID = clientId
-	app.Config.RedirectURL = redirectUrl
-  	app.Config.ClientSecret = clientSecret
-  	app.TenantId = tenantId
-	app.State = RandStringRunes(255)
-	SessionStore.Options = &sessions.Options{
-		HttpOnly: true,
-	}
-
-  	if len(os.Getenv("TENANT_ID")) > 1 {
-    		app.Config.Endpoint = microsoft.AzureADEndpoint(tenantId)
-  	}
-
-	log.Debug("app", app)
-	log.Debug("session store", SessionStore)
-
+func Serve() {
 	http.HandleFunc("/oauth2callback", azureADCallbackHandler)
 	http.HandleFunc("/login", azureADLoginHandler)
 	http.HandleFunc("/logout", logoutHandler)
 	http.HandleFunc("/secret", secretHandler)
 	http.HandleFunc("/", publicHandler)
-
 	http.ListenAndServe(":8080", gcontext.ClearHandler(http.DefaultServeMux))
 }
 
-func main() {
+func init() {
 	rand.Seed(time.Now().UnixNano())
+
+	// set the maxLength of the cookies stored on the disk to a larger number to prevent issues with:
+	// securecookie: the value is too long
+	// when using OpenID Connect , since this can contain a large amount of extra information in the id_token
+
+	// Note, when using the FilesystemStore only the session.ID is written to a browser cookie, so this is explicit for the storage on disk
+	Store.MaxLength(math.MaxInt64)
+}
+
+func main() {
 	log.SetLevel(log.DebugLevel)
 
-	// Gob encoding for gorilla/sessions
+	// gob encoding for gorilla/sessions
 	gob.Register(&oauth2.Token{})
-	gob.Register(&Profile{})
 
-	Serve(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), os.Getenv("REDIRECT_URL"), os.Getenv("TENANT_ID"))
+	// configure the application properties
+	App.Config.ClientID = os.Getenv("CLIENT_ID")
+	App.Config.RedirectURL = os.Getenv("REDIRECT_URL")
+	App.Config.ClientSecret = os.Getenv("CLIENT_SECRET")
+	App.TenantId = os.Getenv("TENANT_ID")
+	App.State = RandStringRunes(255)
+	// sessionStore.Options = &sessions.Options{
+	// 	HttpOnly: true,
+	// }
+	if len(os.Getenv("TENANT_ID")) > 1 {
+		App.Config.Endpoint = microsoft.AzureADEndpoint(App.TenantId)
+	}
+	log.Debug("App: ", App)
+
+	Serve()
 }
